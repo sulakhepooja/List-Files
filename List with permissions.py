f13f4d20c8363976b2f469a41182fa77b5bc383a
sulakhepooja <39737116+sulakhepooja@users.noreply.github.com>
2018-06-26 21:35:48 +0530

    Create List with permissions.py

---
+++ List with permissions.py
@@ -0,0 +1,18 @@
+import os , stat
+permissionDict = {
+    'access': {
+        '0': ('---'),
+        '1': ('--x'),
+        '2': ('-w-'),
+        '3': ('-wx'),
+        '4': ('r--'),
+        '5': ('r-x'),
+        '6': ('rw-'),
+        '7': ('rwx')},}
+for file in os.listdir("/etc"):
+    if file.endswith(".d") or file.endswith(".conf") or file.endswith(".cnf"):
+        print(os.path.join("/etc", file))
+        permissionOctal = oct(os.lstat("/etc").st_mode)[-3:]
+        for role, octal in enumerate(permissionOctal):
+            print( permissionDict['access'][octal])
+
