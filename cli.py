7dd3d3b4dd272f353085a6f876b45a51ffcd3b8f
sulakhepooja <39737116+sulakhepooja@users.noreply.github.com>
2018-07-26 20:15:48 +0530

    Delete cli.py

--- cli.py
+++
@@ -1,9 +0,0 @@
-import sys,os
-for file in os.listdir("/etc"):
-    input = open(os.path.join("/etc", file), "r")
-    content = input.read()
-    arg = sys.argv[0]
-    i=1
-    while i<len(arg):
-        print(arg[i], arg[i+1], content)
-        i+=2
