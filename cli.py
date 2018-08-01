710a969be16f60ce4032022207ac588287310efd
sulakhepooja <39737116+sulakhepooja@users.noreply.github.com>
2018-08-01 19:24:28 +0530

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,11 +1,12 @@
 import sys,os
-for file in os.listdir("/etc"):
-    input = open(os.path.join("/etc", file), "r")
+for file in os.listdir("/proc"):
+    input = open(os.path.join("/proc", file), "r")
     content = input.read()
     arg = sys.argv[0]
     i=1
     while i<len(arg):
         print(arg[i], arg[i+1], content)
+        i+=2
         if(sys.argv[0]== '-c'):
             print(os.path.exists('/proc/cpuinfo'))
         if (sys.argv[0] == '-m'):
@@ -16,4 +17,3 @@ for file in os.listdir("/etc"):
             print(os.path.exists('/proc/cpuinfo'))
         if (sys.argv[0] == '--help'):
             print("This is Help Message")
-        i += 2
